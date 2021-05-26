1b239416bb3ef83592f21908dc371e85b595e24d
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-05-26 20:42:33 -0300

    view

--- js/views/NegociacoesView.js
+++ js/views/NegociacoesView.js
@@ -1,5 +1,11 @@
 class NegociacoesView {
-    template() {
+    constructor(seletor) {
+        this._elemento = document.querySelector(seletor);
+    }
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+    template(model) {
         return `
     <table class="table table-hover table-bordered">
         <thead>
@@ -12,6 +18,19 @@ class NegociacoesView {
         </thead>
 
         <tbody>
+            ${model
+            .paraArray()
+            .map((negociacao) => {
+            return `
+                <tr>
+                    <td>${`${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}`}</td>
+                    <td>${negociacao.quantidade}</td>
+                    <td>${negociacao.valor}</td>
+                    <td>${negociacao.volume}</td>
+                </tr>
+                `;
+        })
+            .join('')}
         </tbody>
 
         <tfoot>
